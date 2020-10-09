8fc4cb6c72d9fa58201a8ee9c63b8323f690cae8
Alex Mccaskey <mccaskeyaj@ornl.gov>
2020-10-09 13:37:45 -0400

    updating qcor bottle
    Signed-off-by: Alex Mccaskey <mccaskeyaj@ornl.gov>

--- qcor.rb
+++ qcor.rb
@@ -3,7 +3,7 @@ class Qcor < Formula
   homepage "https://github.com/ornl-qci/qcor"
   url "https://github.com/ornl-qci/qcor/tarball/master"
   version "1.0.0"
-  sha256 "db7e48bb3628df7230e14b24668f7ba73bd7d5a4d88bfc942d6fa31ed8bd37fc"
+  sha256 "c0cf8262f9a3545d565dd66733b16b948d68dd0f5f38a444162d1c55d65206bb"
   license "BSD-3"
 
   depends_on "cmake"
@@ -14,16 +14,26 @@ class Qcor < Formula
 
   bottle do
     root_url "https://dl.bintray.com/amccaskey/qci-homebrew-bintray"
-    sha256 "a7689d62e9f20b35aeaf2941eb24bd5be8579d264dad3d1355ced284cb872188" => :mojave
+    sha256 "2d33182ef13d08b03bec30c42185d073e240506ff4116b13d8e2e12508340036" => :mojave
   end
 
   def install
+    args = %W[
+      -DCMAKE_BUILD_TYPE=Release
+      -DCMAKE_CXX_COMPILER=/usr/local/opt/gcc/bin/g++-10
+      -DCMAKE_C_COMPILER=/usr/local/opt/gcc/bin/gcc-10
+      -DXACC_DIR=#{prefix}
+      -DQCOR_EXTRA_HEADERS="/usr/local/opt/gcc/include/c++/10.2.0;/usr/local/opt/gcc/include/c++/10.2.0/x86_64-apple-darwin18"
+      -DGCC_STDCXX_PATH=/usr/local/opt/gcc/lib/gcc/10
+      -DLLVM_ROOT=/usr/local/opt/llvm-csp 
+      -G Ninja
+    ]
+
     system "sh", "scripts/macosx/homebrew_build_xacc_and_qcor.sh", "#{prefix}", "cmake"
-  end
-  def caveats
-    <<~EOS
-      To use qcor, ensure that #{opt_prefix}/bin is in your PATH:
-        export PATH=$PATH:#{opt_prefix}/bin 
-    EOS
+    qcorpath = buildpath
+    mkdir qcorpath/"build" do
+       system "cmake", "..", *(std_cmake_args + args)
+       system "cmake", "--build", ".", "--target", "install"
+    end
   end
 end
