b0870be6b371fbbce2671bfa4398be52c5794d22
Alex Mccaskey <mccaskeyaj@ornl.gov>
2020-10-09 10:54:08 -0400

    adding updated qcor formula
    Signed-off-by: Alex Mccaskey <mccaskeyaj@ornl.gov>

--- qcor.rb
+++ qcor.rb
@@ -1,6 +1,3 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Qcor < Formula
   desc "quantum-classical c++ compiler"
   homepage "https://github.com/ornl-qci/qcor"
@@ -11,32 +8,17 @@ class Qcor < Formula
 
   depends_on "cmake"
   depends_on "llvm-csp"
-  depends_on "xacc"
   depends_on "gcc@10"
 
   bottle do
     root_url "https://dl.bintray.com/amccaskey/qci-homebrew-bintray"
-    sha256 "692550ca239f36ffbf06956cd8e573b94b498a5b5c3d7f0557474dcefdc50141" => :mojave
+    sha256 "a7689d62e9f20b35aeaf2941eb24bd5be8579d264dad3d1355ced284cb872188" => :mojave
   end
 
   def install
-    args = %W[
-      -DCMAKE_BUILD_TYPE=Release
-      -DCMAKE_CXX_COMPILER=/usr/local/opt/gcc/bin/g++-10
-      -DCMAKE_C_COMPILER=/usr/local/opt/gcc/bin/gcc-10
-      -DXACC_DIR=/usr/local/opt/xacc
-      -DQCOR_EXTRA_HEADERS="/usr/local/opt/gcc/include/c++/10.2.0;/usr/local/opt/gcc/include/c++/10.2.0/x86_64-apple-darwin18"
-      -DGCC_STDCXX_PATH=/usr/local/opt/gcc/lib/gcc/10
-      -DLLVM_ROOT=/usr/local/opt/llvm-csp 
-      -G Ninja
-    ]
-
-    qcorpath = buildpath
-    system "sh", "scripts/macosx/homebrew_build_xacc_and_qcor.sh", #{prefix}, "cmake"
-#    mkdir qcorpath/"build" do
-#       system "cmake", "..", *(std_cmake_args+args)
-#       system "cmake", "--build", ".", "--target", "install"
-    bin.install #{bin}/qcor
-#    end 
+    system "sh", "scripts/macosx/homebrew_build_xacc_and_qcor.sh", "#{prefix}", "cmake"
+    mkdir buildpath/"build" do
+      system "cmake", "--build", ".", "--target", "install"
+    end
   end
 end
