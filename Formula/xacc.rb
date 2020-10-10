d2969de2ff934ace96e1b12161d1212d3110da03
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-10-10 13:58:19 -0400

    updates to xacc for linux homebrew
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- Formula/xacc.rb
+++ Formula/xacc.rb
@@ -6,16 +6,17 @@ class Xacc < Formula
   sha256 "e9cbf89cb434e76ac550d53f10a8dd454a9573d7218b71e64bf3f53595dfb7fe" 
   license "EPL and EDL"
 
-  depends_on "cmake" => :build
-  depends_on "ninja" => :build
-  depends_on "gcc@10" => :build
-  depends_on "python3" => :build
-  depends_on "openssl" => :build
-  depends_on "curl" => :build
+  depends_on "cmake"
+  depends_on "ninja"
+  depends_on "gcc@10"
+  depends_on "python3"
+  depends_on "openssl"
+  depends_on "curl"
 
   bottle do
     root_url "https://dl.bintray.com/amccaskey/qci-homebrew-bintray"
     sha256 "e3ddae7ce55596a27db0fda47ae2aed99357434155c8de7c68cb2fdc61fb1fbc" => :mojave
+    sha256 "5e8474c2200989b95531c1e1c6e136b3b7e470bb0f0230a540fd59d37f2c918e" => :x86_64_linux
   end
 
   def install
