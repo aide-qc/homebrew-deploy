8015b0683f3cbb591ec0b3a86b20111ca427357e
Alex Mccaskey <mccaskeyaj@ornl.gov>
2020-10-16 17:30:08 -0400

    work on getting mojave travis script working
    Signed-off-by: Alex Mccaskey <mccaskeyaj@ornl.gov>

--- scripts/mojave/qcor/build.sh
+++ scripts/mojave/qcor/build.sh
@@ -7,9 +7,9 @@ cd ../../../
 brew tap aide-qc/deploy 
 HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=false brew install qcor --build-bottle --verbose 
 HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=false brew bottle qcor 
-mv qcor--* qcor-1.0.0.mojave.bottle.tar.gz \
+mv qcor--* qcor-1.0.0.mojave.bottle.tar.gz 
 export bottle_sha=$(shasum -a 256 qcor-1.0.0.mojave.bottle.tar.gz | cut -d " " -f 1) 
-sed -i "0,/sha256 \".*\" => :x86_64_linux/s//sha256 \"$bottle_sha\" => :x86_64_linux /" Formula/qcor.rb 
+sed -i '' "s/   sha256 ".*" => :mojave/   sha256 \"$bottle_sha\" => :mojave/g" Formula/qcor.rb
 git config --global user.email "xacc-dev@eclipse.org"
 git config --global user.name "qcor ci bot" 
 git add Formula/qcor.rb && git commit -m "uploading updated qcor bottle sha" 
@@ -17,4 +17,4 @@ git config remote.aideqchbdeploy.url >&- || git remote add -t master aideqchbdep
 git push -f aideqchbdeploy HEAD:master 
 brew install jfrog-cli 
 jfrog bt c --interactive=false --user=amccaskey --key=$JFROG_API_KEY \
-jfrog bt u --override --publish qcor-1.0.0.x86_64_linux.bottle.tar.gz amccaskey/qci-homebrew-bintray/qcor-linux-x86_64/1.0.0 
+jfrog bt u --override --publish qcor-1.0.0.mojave.bottle.tar.gz amccaskey/qci-homebrew-bintray/qcor-mojave/1.0.0 
