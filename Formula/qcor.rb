c18e11c8e7fa7868e788738d4e2ebf055fe8367a
Alex Mccaskey <mccaskeyaj@ornl.gov>
2020-10-16 13:22:34 -0400

    updating qcor mojave bottle sha
    Signed-off-by: Alex Mccaskey <mccaskeyaj@ornl.gov>

--- Formula/qcor.rb
+++ Formula/qcor.rb
@@ -11,7 +11,7 @@ class Qcor < Formula
 
   bottle do
     root_url "https://dl.bintray.com/amccaskey/qci-homebrew-bintray"
-    sha256 "d9d0948d2194aaa8f1d49585e72a922ab288308a5c861d911c2436b5d523afba" => :mojave
+    sha256 "e7b8a4f18abe491cf2f569c83383199cffd881eccbf56b47c4edc7dd46058057" => :mojave
     sha256 "ba73135e551922fad8e75a03b79320b768397f2ae296569d0e3fccbfc457a7e6" => :x86_64_linux   
   end
 
