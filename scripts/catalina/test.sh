538bba61c3b16090ab2b80951d4387fb8b5992de
Alex McCaskey <mccaskeyaj@ornl.gov>
2020-11-21 10:13:43 -0500

    updates to test scripts for mac
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- scripts/catalina/test.sh
+++ scripts/catalina/test.sh
@@ -2,9 +2,6 @@
 
 set -e
 
-cd homebrew-deploy
-git pull 
-
 /bin/bash -c "$(curl -fsSL https://aide-qc.github.io/deploy/install.sh)"
 printf "__qpu__ void f(qreg q) {\n  H(q[0]);\n  Measure(q[0]);\n}\nint main() {\n  auto q = qalloc(1);\n  f(q);\n  q.print();\n}" | qcor -v -qpu qpp -shots 1024 -x c++ - && ./a.out
 printf "from qcor import qjit, qalloc, qreg\n@qjit\ndef bell(q : qreg):\n    H(q[0])\n    CX(q[0], q[1])\n    for i in range(q.size()):\n        Measure(q[i])\nq = qalloc(2)\nbell(q)\nprint('Results')\nprint(q.counts())" | tee test.py && PYTHONPATH=$(qcor -qcor-install):$(qcor -xacc-install) python3 test.py -qpu qpp -shots 1024
