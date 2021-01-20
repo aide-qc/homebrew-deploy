7345208a2a4bfb82d49a02127e7cdfaaf9e3f798
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-01-20 14:19:36 -0500

    update to script
    Signed-off-by: Alex McCaskey <mccaskeyaj@ornl.gov>

--- scripts/update_sha_sums.py
+++ scripts/update_sha_sums.py
@@ -10,10 +10,10 @@ import json, os, subprocess, hashlib, wget
 #   "qcor" : {... same ...}
 #}
  
-def compute_sha256sum(filename):
-    with open(filename,"rb") as f:
-        bytes = f.read() 
-        return hashlib.sha256(bytes).hexdigest()
+#def compute_sha256sum(filename):
+#    with open(filename,"rb") as f:
+#        bytes = f.read() 
+#        return hashlib.sha256(bytes).hexdigest()
         
 # load previous json shasum file, $HOME/.cache_shasums.json
 def load_json_file(filename: str):
@@ -32,11 +32,16 @@ def run_command_silent(cwd, command, *args):
 def run_command(cwd, command, *args):
     commands = [command] + list(args)
     try:
-        result = subprocess.run(commands, cwd=cwd, check=True)
+        result = subprocess.run(commands, cwd=cwd, check=True, stdout=subprocess.PIPE)
+        return result.stdout.splitlines()
     except subprocess.CalledProcessError as e:
         print(e.output)
         print(e.returncode)
         return e.returncode
+
+def compute_sha256sum(filename):
+    result = run_command(os.getcwd(), 'sha256sum', filename)
+    return result[0].decode('utf-8').split(' ')[0]
 
 cache_file_name = os.getenv('HOME')+'/.cached_shasums.json'
 cached_sums = load_json_file(cache_file_name)
@@ -51,16 +56,23 @@ run_command(os.getcwd()+'/homebrew-deploy', 'git', 'pull')
 # compare with previous shasum...
 # if different, pull homebrew-deploy, update xacc.rb
 run_command(os.getcwd()+'/xacc', 'git', 'pull')
-print('creating xacc tar.gz file')
-run_command_silent(os.getcwd(), 'tar', '-czvf', 'xacc-1.0.0.tar.gz', 'xacc')
-
-xacc_src_hash = compute_sha256sum('xacc-1.0.0.tar.gz')
-print('current xacc source hash:', xacc_src_hash)
-print('previous xacc source hash:', cached_sums['xacc']['source'])
-homebrew_changes_occured = False
+run_command(os.getcwd()+'/qcor', 'git', 'pull')
+xacc_commit_hash = run_command(os.getcwd()+'/xacc', 'git', 'rev-parse', 'HEAD')[0].decode('utf-8')
+qcor_commit_hash = run_command(os.getcwd()+'/qcor', 'git', 'rev-parse', 'HEAD')[0].decode('utf-8')
 git_message = ''
-if xacc_src_hash != cached_sums['xacc']['source']:
-    print('not equal, updating homebrew-deploy and uploading new xacc source.')
+
+print('xacc commit hash is {}'.format(xacc_commit_hash))
+print('qcor commit hash is {}'.format(qcor_commit_hash))
+
+homebrew_changes_occured = False
+if xacc_commit_hash != cached_sums['xacc']['git']:
+
+    print('creating xacc tar.gz file')
+    run_command_silent(os.getcwd(), 'tar', '-czvf', 'xacc-1.0.0.tar.gz', 'xacc')
+    xacc_src_hash = compute_sha256sum('xacc-1.0.0.tar.gz')
+    print('current xacc source hash:', xacc_src_hash)
+    print('previous xacc source hash:', cached_sums['xacc']['source'])
+
     # need to update xacc.rb
     xacc_rb_file_lines = []
     with open('homebrew-deploy/Formula/xacc.rb', 'r') as fin:
@@ -79,17 +91,17 @@ if xacc_src_hash != cached_sums['xacc']['source']:
         git_message += 'automated bot updating xacc source hash: {} to {}.'.format(cached_sums['xacc']['source'], xacc_src_hash)
     
     cached_sums['xacc']['source'] = xacc_src_hash
+    cached_sums['xacc']['git'] = xacc_commit_hash
 
     # and then push new xacc source tar with jfrog
-    # run_command(os.getcwd(), 'jfrog', 'bt', 'u', '--override', '--publish', 'xacc-1.0.0.tar.gz', 'amccaskey/qci-homebrew-bintray/xacc-source/xacc-1.0.0')
+    run_command(os.getcwd(), 'jfrog', 'bt', 'u', '--override', '--publish', 'xacc-1.0.0.tar.gz', 'amccaskey/qci-homebrew-bintray/xacc-source/xacc-1.0.0')
 
-run_command(os.getcwd(), 'wget', 'https://github.com/ornl-qci/qcor/tarball/master')
-qcor_src_hash = compute_sha256sum('master')
-print('current qcor source hash:', qcor_src_hash)
-print('previous qcor source hash:', cached_sums['qcor']['source'])
-git_message = ''
-if qcor_src_hash != cached_sums['qcor']['source']:
-    print('not equal, updating homebrew-deploy and uploading new qcor source.')
+
+if qcor_commit_hash != cached_sums['qcor']['git']:
+    run_command(os.getcwd(), 'wget', 'https://github.com/ornl-qci/qcor/tarball/master')
+    qcor_src_hash = compute_sha256sum('master')
+    print('current qcor source hash:', qcor_src_hash)
+    print('previous qcor source hash:', cached_sums['qcor']['source'])
     # need to update xacc.rb
     qcor_rb_file_lines = []
     with open('homebrew-deploy/Formula/qcor.rb', 'r') as fin:
@@ -105,9 +117,10 @@ if qcor_src_hash != cached_sums['qcor']['source']:
     with open('homebrew-deploy/Formula/qcor.rb', 'w') as fout:
         fout.write(new_qcor_rb_file_str)
         homebrew_changes_occured = True
-        git_message += 'automated bot updating qcor source hash: {} to {}.'.format(cached_sums['qcor']['source'], xacc_src_hash)
+        git_message += 'automated bot updating qcor source hash: {} to {}.'.format(cached_sums['qcor']['source'], qcor_src_hash)
 
     cached_sums['qcor']['source'] = qcor_src_hash
+    cached_sums['qcor']['git'] = qcor_commit_hash
 
 # Remove the qcor and xacc tar files
 run_command(os.getcwd(), 'rm', '-rf', 'master')
@@ -189,4 +202,6 @@ if homebrew_changes_occured:
     run_command(os.getcwd()+'/homebrew-deploy', 'git', 'add', 'Formula')
     run_command(os.getcwd()+'/homebrew-deploy', 'git', 'commit', '-m', '"{}"'.format(git_message))
     run_command(os.getcwd()+'/homebrew-deploy', 'git', 'push', 'aideqchbdeploy', 'HEAD:master')
+    run_command(os.getcwd()+'/homebrew-deploy', 'git', 'pull')
+
 
