b1a65c6444542fd3ccf604243295ea717156241c
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-01-28 07:37:09 -0500

    Update llvm-csp.rb

--- Formula/llvm-csp.rb
+++ Formula/llvm-csp.rb
@@ -11,7 +11,7 @@ class LlvmCsp < Formula
 
   bottle do
     root_url "https://github.com/ORNL-QCI/llvm-project-csp/releases/download/1.0.0"
-    sha256 "d49e9662a1ff7e88d3becf6e45264b611e9356ee1379ac874d646536751790a0" => :mojave
+    sha256 "10182d37cde6312a2ca28d89c50b2198b48d90d8ba14cb3bc18ec49379ff5039" => :mojave
     sha256 "a528321df5dfe7d8928873f2410d01d98824b663eb38b7c6dbc98d5c164b6cfb" => :catalina
     sha256 "4a7c6a6d207de796f307cdabde51862b656bf535e8e41625db93e9f861672934" => :x86_64_linux
   end
@@ -22,34 +22,18 @@ class LlvmCsp < Formula
       mlir
     ]
 
-    ENV.libcxx if ENV.compiler == :clang
-
     # ENV.deparallelize  # if your formula fails when building in parallel
     # Remove unrecognized options if warned by configure
     args = %W[
       -DBUILD_SHARED_LIBS=ON
       -DCMAKE_BUILD_TYPE=Release
-      -DCMAKE_C_FLAGS_RELEASE=-DNDEBUG
-      -DCMAKE_CXX_FLAGS_RELEASE=-DNDEBUG
       -DLLVM_TARGETS_TO_BUILD=X86
-      -DLLVM_INCLUDE_TESTS=OFF
       -DLLVM_ENABLE_DUMP=ON
-      -DLLVM_INCLUDE_DOCS=OFF
       -DLLVM_ENABLE_PROJECTS=#{projects.join(";")}
       -DCMAKE_INSTALL_PREFIX=#{prefix}
       -G Ninja
     ]
-
-    if MacOS.version == :mojave && MacOS::CLT.installed?
-      # Mojave CLT linker via software update is older than Xcode.
-      # Use it to retain compatibility.
-      args << "-DCMAKE_LINKER=/Library/Developer/CommandLineTools/usr/bin/ld"
-    end
-
-    sdk = MacOS.sdk_path_if_needed
-    args << "-DDEFAULT_SYSROOT=#{sdk}" if sdk
-    args << "-DCMAKE_OSX_SYSROOT=#{sdk}" if sdk
-
+    
     llvmpath = buildpath
     mkdir llvmpath/"build" do
        system "cmake", "../llvm", *(args)
