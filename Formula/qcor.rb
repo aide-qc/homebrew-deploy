e34cab454ced2342e396c43416886057b6340906
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-03-05 00:45:11 -0500

    "automated bot updating xacc x86_64_linux bottle hash: decbaa6cf90724926ac39e07d337148fc3120d93e694c9eff7af05244bc24512 to 7ef5735f864da4a3b70a63cade1cb2d2aa10660e96a0866f23f57243aeb510b8automated bot updating qcor x86_64_linux bottle hash: af86ac2238d8eaa45c67a11aad5c3b80737fc7c91a31b033bca475c16ec1b9b3 to 35498b6b5baaaaac02a898d109a6b703b756c4b50cc01528755919325609b84b"

--- Formula/qcor.rb
+++ Formula/qcor.rb
@@ -13,7 +13,7 @@ class Qcor < Formula
     root_url "https://dl.bintray.com/amccaskey/qci-homebrew-bintray"
     sha256 "bc5a41dc8400f9da9d0a90e8086d09998759928f3acda23b2f4076a6b255a9d0" => :mojave
     sha256 "95b8be73600ddeac78a88742d18783a0779c9131e34eca8729e7a5d21dabc27f" => :catalina
-    sha256 "af86ac2238d8eaa45c67a11aad5c3b80737fc7c91a31b033bca475c16ec1b9b3" => :x86_64_linux
+    sha256 "35498b6b5baaaaac02a898d109a6b703b756c4b50cc01528755919325609b84b" => :x86_64_linux
   end
 
   def install
