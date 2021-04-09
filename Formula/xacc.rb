6e0ace2de4df9415ae1c81548b87437786972eed
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-04-09 15:41:02 -0400

    "automated bot updating xacc source hash: 48998e581bb77bd84d7b83d8db92443022f42018c82883663b762d33cec9337b to 9d34afa99acd4eaf03d02a978dcadab41517fa21e51488b97076c0ed9df58d4e."

--- Formula/xacc.rb
+++ Formula/xacc.rb
@@ -3,7 +3,7 @@ class Xacc < Formula
   homepage "https://github.com/eclipse/xacc"
   url "https://dl.bintray.com/amccaskey/qci-homebrew-bintray/xacc-1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "48998e581bb77bd84d7b83d8db92443022f42018c82883663b762d33cec9337b"
+  sha256 "9d34afa99acd4eaf03d02a978dcadab41517fa21e51488b97076c0ed9df58d4e"
   license "EPL and EDL"
 
   depends_on "cmake"
