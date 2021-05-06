32c0db488edb008d00b40b48e72f388120a1028e
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-05-06 00:40:07 -0400

    "automated bot updating xacc mojave bottle hash: 5148d6214798322a3425265e73602adbabf66a053dc137a43d83a8d0873cacf8 to c888bf75859f224d2a016d8b1252c64862ff4c87616189b7dfc3c57aa7df943d automated bot updating qcor mojave bottle hash: ce848a1e1e771cd70832db626c5eacec5a47994da4d981b5721e58fc5cf8295d to d7230cc8f7199d7e94e3f6ecac16b83bce30e8a905a04825c13a730c81dbdc7f "

--- Formula/qcor.rb
+++ Formula/qcor.rb
@@ -11,7 +11,7 @@ class Qcor < Formula
 
   bottle do
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
-    sha256 "ce848a1e1e771cd70832db626c5eacec5a47994da4d981b5721e58fc5cf8295d" => :mojave
+    sha256 "d7230cc8f7199d7e94e3f6ecac16b83bce30e8a905a04825c13a730c81dbdc7f" => :mojave
     sha256 "a134ea2c24b48b3d263510a88174da8756b18209a9865cc30ec2d307e996978c" => :catalina
     sha256 "af98db2ac824a38785e47f084004208cf499d0daa9e59e798fcfb9acf71a1135" => :x86_64_linux
   end
--- Formula/xacc.rb
+++ Formula/xacc.rb
@@ -14,7 +14,7 @@ class Xacc < Formula
 
   bottle do
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
-    sha256 "5148d6214798322a3425265e73602adbabf66a053dc137a43d83a8d0873cacf8" => :mojave
+    sha256 "c888bf75859f224d2a016d8b1252c64862ff4c87616189b7dfc3c57aa7df943d" => :mojave
     sha256 "1f1ac6ffacd1ed8e79943318b6289b7d764f1dd7f5feb8c678a5e39f5e6dc074" => :catalina
     sha256 "377dc2090dfffa9ddee6c6f915be44b1ee9f1f6e3a103c4de7d2e5b3e519916e" => :x86_64_linux
   end
