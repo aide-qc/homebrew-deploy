21d59d09eb2f1fbda4f8a6cea2ac2264ded4ff94
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-05-19 00:30:08 -0400

    "automated bot updating xacc x86_64_linux bottle hash: 6243bea343eefc3dec55351e786e25cc000ef29d2d70a516a4f0b21858d5b214 to a5870b75e9c5cefdc4c9ad73bdcfc69dd8d0a7e649177b898c6cd3d3aa9f3a52 automated bot updating qcor x86_64_linux bottle hash: 0369cea3f27d093c0806ff2e76cee2b6f9f1e62446c642ed224911db137e7a0e to 7c9323e0ce25a42d0ebf7cd8b38d8ad4724ce1d9ac28a1d409f7e97f88dd8f30 "

--- Formula/qcor.rb
+++ Formula/qcor.rb
@@ -13,7 +13,7 @@ class Qcor < Formula
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
     sha256 "b6b678408e14549f4cd3d4193451cf8207994e95ac0d5d08a7d6280109705d02" => :mojave
     sha256 "cd1155718e2f94c922b9e094e31b5b3c63136e2c9ecf230e9ef4e796ea83a383" => :catalina
-    sha256 "0369cea3f27d093c0806ff2e76cee2b6f9f1e62446c642ed224911db137e7a0e" => :x86_64_linux
+    sha256 "7c9323e0ce25a42d0ebf7cd8b38d8ad4724ce1d9ac28a1d409f7e97f88dd8f30" => :x86_64_linux
   end
 
   def install
--- Formula/xacc.rb
+++ Formula/xacc.rb
@@ -16,7 +16,7 @@ class Xacc < Formula
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
     sha256 "55aad23f77caf9560897205d14a36462d594a450edc5de87ac74276b9256d1c9" => :mojave
     sha256 "ee0ae57dd532892d307cf95bacee69e7dea82a8d16814a5cac05d2d8e1374667" => :catalina
-    sha256 "6243bea343eefc3dec55351e786e25cc000ef29d2d70a516a4f0b21858d5b214" => :x86_64_linux
+    sha256 "a5870b75e9c5cefdc4c9ad73bdcfc69dd8d0a7e649177b898c6cd3d3aa9f3a52" => :x86_64_linux
   end
 
   def install
