a4c9c2e806e37a1ca01f3fd89ac35f3e0f92a84b
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-06-30 11:30:14 -0400

    "automated bot updating qcor source hash: 5fbc4ef1c671bed00ede4e80c919c0526e1228474bc4c8d3bf5e88f225a612df to 03a47ce0155f4cbad62048c75507eea376c9ff1eb31e8ab61aea6bf341cf805a. "

--- Formula/qcor.rb
+++ Formula/qcor.rb
@@ -3,14 +3,14 @@ class Qcor < Formula
   homepage "https://github.com/ornl-qci/qcor"
   url "https://github.com/ornl-qci/qcor/tarball/master"
   version "1.0.0"
-  sha256 "5fbc4ef1c671bed00ede4e80c919c0526e1228474bc4c8d3bf5e88f225a612df"
+  sha256 "03a47ce0155f4cbad62048c75507eea376c9ff1eb31e8ab61aea6bf341cf805a"
   license "BSD-3"
 
   bottle do
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
-    sha256 mojave: "98fffa08276e99bf9d39250af1cab726b8fc4971a2e1881f69fdefc9088c5bef"
-    sha256 catalina: "eaf77bc661ec2ec60618466e0f615a23c8eb97d7d7c19d54e6d229d199be88e0"
-    sha256 x86_64_linux: "c87b091f9b6f08939065ab874fc7669e977defb9e4bc3f65194396c6bd530aaa"
+  sha256 "03a47ce0155f4cbad62048c75507eea376c9ff1eb31e8ab61aea6bf341cf805a"
+  sha256 "03a47ce0155f4cbad62048c75507eea376c9ff1eb31e8ab61aea6bf341cf805a"
+  sha256 "03a47ce0155f4cbad62048c75507eea376c9ff1eb31e8ab61aea6bf341cf805a"
   end
 
   depends_on "llvm-csp"
