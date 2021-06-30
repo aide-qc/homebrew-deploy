8a2e09a9a8a5a7434d955387abe20df557ec971d
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-06-30 13:22:58 -0400

    "automated bot updating xacc source hash: 749989dfc2c778523d25396eaf0f0ad447986e1a8548b589e7c49301e6036c4f to 4c3fc3acfcf9380da7ee3a9e9151cdcebc5fa2fb90367cbd0ccfebbca65853bb. "

--- Formula/xacc.rb
+++ Formula/xacc.rb
@@ -3,14 +3,14 @@ class Xacc < Formula
   homepage "https://github.com/eclipse/xacc"
   url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1/xacc-1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "749989dfc2c778523d25396eaf0f0ad447986e1a8548b589e7c49301e6036c4f"
+  sha256 "4c3fc3acfcf9380da7ee3a9e9151cdcebc5fa2fb90367cbd0ccfebbca65853bb"
   license "EPL and EDL"
 
   bottle do
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
-    sha256 mojave: "edeae6f85e8272b20b8600973ec2e9c4445ad477d963c3fe4792c4101773d244"
-    sha256 catalina: "386f468c8421c3b501070257f5c15b9d50a7ed7ef78a2f2ab9d6aee2e10c2d3f"
-    sha256 x86_64_linux: "fcfcc4e1e27b15a9f183fbf15d753ca087e9b212e49250df70e7e6f953b2fc9d"
+  sha256 "4c3fc3acfcf9380da7ee3a9e9151cdcebc5fa2fb90367cbd0ccfebbca65853bb"
+  sha256 "4c3fc3acfcf9380da7ee3a9e9151cdcebc5fa2fb90367cbd0ccfebbca65853bb"
+  sha256 "4c3fc3acfcf9380da7ee3a9e9151cdcebc5fa2fb90367cbd0ccfebbca65853bb"
   end
 
   depends_on "cmake"
