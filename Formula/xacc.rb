4cc133c6817a28212890a08dced598468b698e84
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-07-08 00:30:09 -0400

    "automated bot updating xacc x86_64_linux bottle hash: dfacd611a6ee30daaf74cfb9983b8605f9b2a5658a723e079fd9618bbcf5e98a to 42370925822beeec1e223721671eb5b732149bb131d286208b940e0a0e5d1a07 automated bot updating qcor x86_64_linux bottle hash: b4a563f663e9e9abf7fe6e264f183aa794fd3b9c5177cd713f0368429a52dab7 to 23665788ea3e4c2753062c6c89577d551feb016c254f7847e4105e63840ade5f "

--- Formula/xacc.rb
+++ Formula/xacc.rb
@@ -10,7 +10,7 @@ class Xacc < Formula
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
     sha256 catalina: "386f468c8421c3b501070257f5c15b9d50a7ed7ef78a2f2ab9d6aee2e10c2d3f"
     sha256 mojave: "edeae6f85e8272b20b8600973ec2e9c4445ad477d963c3fe4792c4101773d244"
-    sha256 x86_64_linux: "dfacd611a6ee30daaf74cfb9983b8605f9b2a5658a723e079fd9618bbcf5e98a"
+    sha256 x86_64_linux: "42370925822beeec1e223721671eb5b732149bb131d286208b940e0a0e5d1a07"
   end
 
   depends_on "cmake"
