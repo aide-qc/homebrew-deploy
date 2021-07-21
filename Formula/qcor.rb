731233b644585644ef3b81afb5af2bebb7e25ad9
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-07-21 00:45:10 -0400

    "automated bot updating xacc mojave bottle hash: 35e836d1c97f365df8d566d6d7f17013b2c2593656e010cef31736b6c4000548 to 8baa1674bbd0279f103d69ff0f339b9b450f65decf5c176bffc4dd895a8f4839 automated bot updating qcor mojave bottle hash: d1cdae8d363445f5fd9fc59d954f51309c63f92a5c8d9f02be1ff55fe9fed1f1 to d7145f10c9dfce3487aa7ec66cd168cb4aa42089cf1d8ed46d9739e9831a72b2 "

--- Formula/qcor.rb
+++ Formula/qcor.rb
@@ -9,7 +9,7 @@ class Qcor < Formula
   bottle do
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
     sha256 catalina: "2a9fa047200a31c6ace6ead690be6cb6eaad511d4db51dd993c41e3c7b710512"
-    sha256 mojave: "d1cdae8d363445f5fd9fc59d954f51309c63f92a5c8d9f02be1ff55fe9fed1f1"
+    sha256 mojave: "d7145f10c9dfce3487aa7ec66cd168cb4aa42089cf1d8ed46d9739e9831a72b2"
     sha256 x86_64_linux: "5dcb4832eed00ac6de9052c896193db42e767f4cfa1f547c756021f1ee8c9756"
   end
 
