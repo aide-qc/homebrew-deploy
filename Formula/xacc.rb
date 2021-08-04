008b9abfea22b704a2af30d9ad6d6af32021397d
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-08-04 11:35:56 -0400

    "automated bot updating xacc source hash: bcf60eaa98f5e941ec2950ccf59cb37c36e7933a45e587fb8728ab414ee13b9e to 9320c432d656d0dc23d179ac31356237e1845f09d64117aed2690782194c2239. "

--- Formula/xacc.rb
+++ Formula/xacc.rb
@@ -3,7 +3,7 @@ class Xacc < Formula
   homepage "https://github.com/eclipse/xacc"
   url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1/xacc-1.0.0.tar.gz"
   version "1.0.0"
-  sha256 "bcf60eaa98f5e941ec2950ccf59cb37c36e7933a45e587fb8728ab414ee13b9e"
+  sha256 "9320c432d656d0dc23d179ac31356237e1845f09d64117aed2690782194c2239"
   license "EPL and EDL"
 
   bottle do
