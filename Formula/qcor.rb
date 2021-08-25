59e4ef2414e19a861ac18b54ea3f68446bfa97fb
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-08-25 00:55:07 -0400

    "automated bot updating xacc catalina bottle hash: 0734c76d6eef1299114b0978da1c05cbb68036e62bc619af33bba112b931f06c to 7c9d1274c2028c6bf1475310345923791e5fbf87b6c3bb8c90b32772c73eb6bc automated bot updating qcor catalina bottle hash: 48cdc448ba3453eb1574d054dccddcd0660fa14e494ca329447fb9f448b36ba2 to 6ce2230428e2289251b28cb23ef9a4be1e384cf8df08eb4c64409a17ed1425f4 "

--- Formula/qcor.rb
+++ Formula/qcor.rb
@@ -8,7 +8,7 @@ class Qcor < Formula
 
   bottle do
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
-    sha256 catalina: "48cdc448ba3453eb1574d054dccddcd0660fa14e494ca329447fb9f448b36ba2"
+    sha256 catalina: "6ce2230428e2289251b28cb23ef9a4be1e384cf8df08eb4c64409a17ed1425f4"
     sha256 mojave: "67b355e009c292edf16d209bc5bbf68dbedef926b547eae275ac1641e77edeea"
     sha256 x86_64_linux: "daee12fc636bef129ad8e29e5a5797b1febd304f408e8009d8e4b6b1796b5d0f"
   end
--- Formula/xacc.rb
+++ Formula/xacc.rb
@@ -8,7 +8,7 @@ class Xacc < Formula
 
   bottle do
     root_url "https://github.com/aide-qc/aide-qc/releases/download/0.0.1"
-    sha256 catalina: "0734c76d6eef1299114b0978da1c05cbb68036e62bc619af33bba112b931f06c"
+    sha256 catalina: "7c9d1274c2028c6bf1475310345923791e5fbf87b6c3bb8c90b32772c73eb6bc"
     sha256 mojave: "b13bce5a35fbebd4677261adc3c5f4e8d46aaa96b092a24ea3cfa1cda158e0c4"
     sha256 x86_64_linux: "fce310c481e9ffd9965c676afd7c1a197af54dd94a0377ce6689198ccaa7ab9a"
   end
