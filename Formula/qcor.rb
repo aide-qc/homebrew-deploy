bd7a328dfb3eb72ce04f7f342b4aa2f8b42be693
Alex McCaskey <mccaskeyaj@ornl.gov>
2021-09-10 14:20:11 -0400

    "automated bot updating qcor source hash: 512abbfadabb90ce784f8d33ffd7e433bcbb4354db6ce56290dcbc6f951ccc0e to b8bd5184c6553a842950c1ad1178c34d1a4f387a8e394cc3df3443bb14ba60b0. "

--- Formula/qcor.rb
+++ Formula/qcor.rb
@@ -3,7 +3,7 @@ class Qcor < Formula
   homepage "https://github.com/ornl-qci/qcor"
   url "https://github.com/ornl-qci/qcor/tarball/master"
   version "1.0.0"
-  sha256 "512abbfadabb90ce784f8d33ffd7e433bcbb4354db6ce56290dcbc6f951ccc0e"
+  sha256 "b8bd5184c6553a842950c1ad1178c34d1a4f387a8e394cc3df3443bb14ba60b0"
   license "BSD-3"
 
   bottle do
